54a3fa85be8a08f47c92b9e2ee56b3a9541f6d28
timegowhere <1653942463@qq.com>
2016-11-02 17:56:05 +0800

    实现ripple效果

--- ripple/src/main/java/com/imooc/skybatua/ripple/MainActivity.java
+++ ripple/src/main/java/com/imooc/skybatua/ripple/MainActivity.java
@@ -1,13 +1,39 @@
 package com.imooc.skybatua.ripple;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Outline;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        tv4 = (TextView) findViewById(R.id.tv4);
+//        CircleOutlineProvider circleOutlineProvider = new CircleOutlineProvider();
+//        tv4.setOutlineProvider(circleOutlineProvider);
+//        ImageView iv = (ImageView) findViewById(R.id.iv);
+//
+//        AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.property_animator);
+//        animatorSet.setTarget(iv);
+//        animatorSet.start();
+
+    }
+    private class CircleOutlineProvider extends ViewOutlineProvider{
+
+        @Override
+        public void getOutline(View view, Outline outline) {
+            outline.setOval(0,0,view.getWidth(),view.getHeight());
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
